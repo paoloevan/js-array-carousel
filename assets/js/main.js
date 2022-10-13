5fddca3ecfa05f137631989cd7d7c4782dd1d1a7
paoloevan <paoloev1900@gmail.com>
2022-10-13 20:46:06 +0200

    add bonus 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,13 @@
 /*
-MILESTONE 1
-creiamo il markup statico: costruiamo il container e inseriamo un'immagine
-grande al centro
+BONUS 1:
+Aggiungere il ciclo infinito del carosello.
+Ovvero se è attiva la prima immagine e l'utente clicca la freccia
+per andare all’immagine precedente, dovrà comparire l’ultima immagine
+dell’array e viceversa.
 
-MILESTONE 2
-Adesso rimuoviamo tutto il markup statico
-e inseriamo tutte le immagini dinamicamente servendoci dell'array fornito
-e un semplice ciclo for che concatena un template literal.
-Tutte le immagini saranno nascoste, tranne la prima,
-che avrà una classe specifica che la renderà visibile.
-Al termine di questa fase ci ritroveremo con lo stesso slider stilato
-nella milestone 1, ma costruito dinamicamente attraverso JavaScript.
-
-MILESTONE 3
-Al click dell'utente sulle frecce, il programma cambierà l’immagine attiva,
-che quindi verrà visualizzata al posto della precedente.
+BONUS 2:
+Aggiungere la visualizzazione di tutte le thumbnails sulla destra dell’immagine grande attiva, come nello screenshot proposto. Tutte le miniature avranno un layer di opacità scura, tranne quella corrispondente all’immagine attiva, che invece avrà un bordo colorato. Al click delle frecce, oltre al cambio di immagine attiva, gestire il cambio di miniatura attiva.
+Prima di partire a scrivere codice: Non lasciamoci spaventare dalla complessità apparente dell'esercizio, ma analizziamo prima, come abbiamo fatto sempre, cosa ci potrebbe aspettare. Abbiamo completato ormai da qualche giorno la sessione HTML e CSS, se non ci ricordiamo qualcosa andiamo pure a riguardare alcuni argomenti. Non dedichiamo però al ripasso più di una mezz'ora, così da non perdere di vista il focus dell'esercizio.
 */
 
 
@@ -54,11 +47,18 @@ nextEl.addEventListener('click', function () {
     //tolgo la classe active
     currentImg.classList.remove('active')
     //incremento activeImg
-    activeImg++
+    if (activeImg == 4) {
+        activeImg = 0
+    } else {
+        activeImg++
+    }
+    console.log(activeImg);
+
     //seleziono tutte le immagini
     const allSlides = document.getElementsByClassName('img_fluid');
     //seleziono la prossima immagine
     const nextSlide = allSlides[activeImg];
+    console.log(nextSlide, 'iiiiii');
     // aggiungo alla slide successiva la classe active
     nextSlide.classList.add('active');
 
@@ -70,7 +70,11 @@ prevEl.addEventListener('click', function () {
     //tolgo la classe active
     currentImg.classList.remove('active')
     //incremento activeImg
-    activeImg--
+    if (activeImg == 0) {
+        activeImg = 4
+    } else {
+        activeImg--
+    }
     //seleziono tutte le immagini
     const allSlides = document.getElementsByClassName('img_fluid');
     //seleziono la prossima immagine
